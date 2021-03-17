edf9a2e03656841375efb1971ccd3b7a2795a4a4
Catarina <c.alexandracorreia@ua.pt>
2021-03-17 17:38:13 +0000

    Initial setup of the project

--- dataRetention/app/views.py
+++ dataRetention/app/views.py
@@ -1,3 +1,12 @@
+import logging
+
 from django.shortcuts import render
 
-# Create your views here.
+logger = logging.getLogger(__name__)
+
+
+'''
+    Initial page just to init the demo
+'''
+def index(request):
+    return render(request, 'index.html')
